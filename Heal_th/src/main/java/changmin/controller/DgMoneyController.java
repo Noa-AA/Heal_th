459076233c806663d262856a7e80029e2bc00d2e
Changmin <kcm2749@naver.com>
2022-11-21 21:06:36 +0900

    221121 - changmin - test: 폴더구조 머지 테스트

--- Heal_th/src/main/java/changmin/controller/DgMoneyController.java
+++ Heal_th/src/main/java/changmin/controller/DgMoneyController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import changmin.service.face.DgMoneyService;
 
@@ -13,4 +14,9 @@ public class DgMoneyController {
 	private Logger logger = LoggerFactory.getLogger(DgMoneyController.class);
 	
 	@Autowired private DgMoneyService dgMoneyService;
+	
+	@RequestMapping(value = "/dgmoney/charge")
+	public void moneyCharge() {
+		logger.info("/dgmoney/charge [GET]");
+	}
 }
