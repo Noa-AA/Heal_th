bda91d1946e4ae3ad5a8f9d4ede6c32d77f02f7c
Yerim <dpfla725@naver.com>
2022-11-23 13:57:28 +0900

    2022-11-13 before write codes

--- Heal_th/src/main/java/yerim/controller/LoginController.java
+++ Heal_th/src/main/java/yerim/controller/LoginController.java
@@ -1,5 +1,7 @@
 package yerim.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -8,7 +10,7 @@ import yerim.service.face.LoginService;
 @Controller
 public class LoginController {
  @Autowired LoginService loginService;
-	
+ private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 
 }
