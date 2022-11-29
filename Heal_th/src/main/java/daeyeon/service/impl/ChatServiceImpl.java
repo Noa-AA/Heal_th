3536081c2c8d369cb78b1d530803a6407ac6c588
82109 <82109@112.170.156.148>
2022-11-29 21:15:08 +0900

    221129_daeyeon_upload

--- Heal_th/src/main/java/daeyeon/service/impl/ChatServiceImpl.java
+++ Heal_th/src/main/java/daeyeon/service/impl/ChatServiceImpl.java
@@ -1,10 +1,61 @@
 package daeyeon.service.impl;
 
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import daeyeon.dao.face.ChatDao;
+import daeyeon.dto.RoomList;
+import daeyeon.dto.Userss;
 import daeyeon.service.face.ChatService;
 
 @Service
 public class ChatServiceImpl implements ChatService {
 
+@Autowired private ChatDao chatDao;
+	
+	//로그 객체
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	
+	@Override
+	public RoomList selectRoomNoByUserNo(HttpSession session) {
+		Userss users = new Userss();
+		
+		RoomList roomList = new RoomList();
+		
+		//세션에있는 유저넘버값
+		logger.info("userNo = {}", session.getAttribute("userNo") );
+		
+		users.setUserNo( (Integer)session.getAttribute("userNo") );
+		
+		roomList = chatDao.selectRoomNoByUserNo(users);
+		
+		logger.info("roomlist = {}", roomList);
+		
+		return roomList;
+	}
+	
+//	@Override
+//	public void createRoom() {
+//		logger.info("createRoom()");
+		
+//		chatDao.insertChatRoom(); 
+//		
+//		chatDao.insertroomList(); //매개변수로 세션의 유저번호 들어가야함
+		
+//	}
+	
+	
+	@Override
+		public void insertChat() {
+			logger.info("insertChat()");
+		
+			chatDao.insertChat(); 
+			
+		}
+	
 }
