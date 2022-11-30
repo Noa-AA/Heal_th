52887a557bde4a7b582ae8e617b1d73b10df13d2
차현경 <user@DESKTOP-HHVLQG9>
2022-11-30 21:23:36 +0900

    221130-차현경-기본틀

--- Heal_th/src/main/java/hyunkyung/dto/Challenge.java
+++ Heal_th/src/main/java/hyunkyung/dto/Challenge.java
@@ -1,5 +1,90 @@
 package hyunkyung.dto;
 
-public class Challenge {
 
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class Challenge {
+	private int challengeNo;
+	private String challengeKind;
+	private String challengeName;
+	private Date challengeCredate;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date challengeEnddate;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private int mNo;
+	
+	public Challenge() {
+	}
+
+	public Challenge(int challengeNo, String challengeKind, String challengeName, Date challengeCredate,
+			Date challengeEnddate, int mNo) {
+		super();
+		this.challengeNo = challengeNo;
+		this.challengeKind = challengeKind;
+		this.challengeName = challengeName;
+		this.challengeCredate = challengeCredate;
+		this.challengeEnddate = challengeEnddate;
+		this.mNo = mNo;
+	}
+
+	@Override
+	public String toString() {
+		return "Challenge [challengeNo=" + challengeNo + ", challengeKind=" + challengeKind + ", challengeName="
+				+ challengeName + ", challengeCredate=" + challengeCredate + ", challengeEnddate=" + challengeEnddate
+				+ ", mNo=" + mNo + "]";
+	}
+
+	public int getChallengeNo() {
+		return challengeNo;
+	}
+
+	public void setChallengeNo(int challengeNo) {
+		this.challengeNo = challengeNo;
+	}
+
+	public String getChallengeKind() {
+		return challengeKind;
+	}
+
+	public void setChallengeKind(String challengeKind) {
+		this.challengeKind = challengeKind;
+	}
+
+	public String getChallengeName() {
+		return challengeName;
+	}
+
+	public void setChallengeName(String challengeName) {
+		this.challengeName = challengeName;
+	}
+
+	public Date getChallengeCredate() {
+		return challengeCredate;
+	}
+
+	public void setChallengeCredate(Date challengeCredate) {
+		this.challengeCredate = challengeCredate;
+	}
+
+	public Date getChallengeEnddate() {
+		return challengeEnddate;
+	}
+
+	public void setChallengeEnddate(Date challengeEnddate) {
+		this.challengeEnddate = challengeEnddate;
+	}
+
+	public int getmNo() {
+		return mNo;
+	}
+
+	public void setmNo(int mNo) {
+		this.mNo = mNo;
+	}
+	
+	
+	
+	
 }
---
+++ Heal_th/src/main/java/hyunkyung/service/face/CreateService.java
@@ -0,0 +1,14 @@
+package hyunkyung.service.face;
+
+import hyunkyung.dto.Challenge;
+
+public interface CreateService {
+
+	/**
+	 * 챌린지 만들기
+	 * 
+	 * @param challenge - 입력될 챌린지 정보
+	 */
+	public void create(Challenge challenge);
+
+}
