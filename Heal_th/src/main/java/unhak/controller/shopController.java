b7d8d1ff7826cac7f457a2e56ed2f821d6351112
Win10 <Win10@DESKTOP-0JCE4Q0>
2022-11-30 21:02:49 +0900

    운학 - 기본구조

--- Heal_th/src/main/java/unhak/controller/shopController.java
+++
@@ -1,8 +0,0 @@
-package unhak.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class shopController {
-
-}
--- Heal_th/src/main/java/unhak/service/impl/TestServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package unhak.service.impl;
-
-import unhak.service.face.TestService;
-
-public class TestServiceImpl implements TestService {
-
-}
