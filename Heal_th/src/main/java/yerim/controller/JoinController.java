a5ef727cba478e991635eef9dbf2156f7b1fc0db
Yerim <dpfla725@naver.com>
2022-12-08 16:30:13 +0900

    20221208_yerim_문자보내기 API 클래스 새로 생성

--- Heal_th/src/main/java/yerim/controller/JoinController.java
+++ Heal_th/src/main/java/yerim/controller/JoinController.java
@@ -65,13 +65,16 @@ public class JoinController {
 	 
 	 @ResponseBody
 	 @PostMapping("/login/userChk")
-	 public	 SmsResponse sendMsg(Users userPhone,HttpSession session) {
+	 public	 String sendMsg(Users userPhone,HttpSession session) {
 		 logger.info("문자 요청 {}",userPhone.getUserPhone());
 		 
 		 //문자 요청
-		 SmsResponse message = joinService.sendRan(userPhone,session);
+		 String message = joinService.sendRan(userPhone,session);
 		 logger.info("{}",message);
 		 
+		 //인증번호 세션에 저장
+		 session.setAttribute("message", message);
+			
 		 //문자요청 응답 반환
 		 return message;
 	 }
