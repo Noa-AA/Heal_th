c5cd16a06befc02b84e3ed71d65b7fe4b32c3df7
JJUFE <JJUFE@JJUFE>
2022-12-13 20:02:29 +0900

    221213-jucheol- 스크랩중

---
+++ Heal_th/src/main/java/jucheol/service/face/ScrapService.java
@@ -0,0 +1,14 @@
+package jucheol.service.face;
+
+import jucheol.dto.Scrap;
+
+public interface ScrapService {
+
+	/**
+	 * 회원번호를 받고 글을 저장
+	 * 
+	 * @param scrap
+	 */
+	public void addScrap(Scrap scrap);
+
+}
