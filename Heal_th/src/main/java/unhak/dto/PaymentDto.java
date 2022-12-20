814f03a027b3066a3664ec60f29c1411bd19860b
Daeyeon <Daeyeon@DESKTOP-FC08J8L>
2022-12-20 20:01:44 +0900

    1220_daeyeon_adminPayment

--- Heal_th/src/main/java/unhak/dto/PaymentDto.java
+++ Heal_th/src/main/java/unhak/dto/PaymentDto.java
@@ -28,21 +28,12 @@ public class PaymentDto {
 	/*상품메인사진*/
 	private String pImage1;
 	
+	private int rnum;
 	
 	public PaymentDto() {	}
 
-
-	@Override
-	public String toString() {
-		return "PaymentDto [payNo=" + payNo + ", orderNo=" + orderNo + ", paymentMethod=" + paymentMethod
-				+ ", userCardcom=" + userCardcom + ", paymentAmount=" + paymentAmount + ", paymentDate=" + paymentDate
-				+ ", address=" + address + ", phoneNo=" + phoneNo + ", userNo=" + userNo + ", pName=" + pName
-				+ ", pImage1=" + pImage1 + "]";
-	}
-
-
 	public PaymentDto(int payNo, String orderNo, String paymentMethod, String userCardcom, int paymentAmount,
-			Date paymentDate, String address, int phoneNo, int userNo, String pName, String pImage1) {
+			Date paymentDate, String address, int phoneNo, int userNo, String pName, String pImage1, int rnum) {
 		super();
 		this.payNo = payNo;
 		this.orderNo = orderNo;
@@ -55,117 +46,115 @@ public class PaymentDto {
 		this.userNo = userNo;
 		this.pName = pName;
 		this.pImage1 = pImage1;
+		this.rnum = rnum;
 	}
 
+	@Override
+	public String toString() {
+		return "PaymentDto [payNo=" + payNo + ", orderNo=" + orderNo + ", paymentMethod=" + paymentMethod
+				+ ", userCardcom=" + userCardcom + ", paymentAmount=" + paymentAmount + ", paymentDate=" + paymentDate
+				+ ", address=" + address + ", phoneNo=" + phoneNo + ", userNo=" + userNo + ", pName=" + pName
+				+ ", pImage1=" + pImage1 + ", rnum=" + rnum + "]";
+	}
 
 	public int getPayNo() {
 		return payNo;
 	}
 
-
 	public void setPayNo(int payNo) {
 		this.payNo = payNo;
 	}
-
 
 	public String getOrderNo() {
 		return orderNo;
 	}
 
-
 	public void setOrderNo(String orderNo) {
 		this.orderNo = orderNo;
 	}
-
 
 	public String getPaymentMethod() {
 		return paymentMethod;
 	}
 
-
 	public void setPaymentMethod(String paymentMethod) {
 		this.paymentMethod = paymentMethod;
 	}
-
 
 	public String getUserCardcom() {
 		return userCardcom;
 	}
 
-
 	public void setUserCardcom(String userCardcom) {
 		this.userCardcom = userCardcom;
 	}
-
 
 	public int getPaymentAmount() {
 		return paymentAmount;
 	}
 
-
 	public void setPaymentAmount(int paymentAmount) {
 		this.paymentAmount = paymentAmount;
 	}
-
 
 	public Date getPaymentDate() {
 		return paymentDate;
 	}
 
-
 	public void setPaymentDate(Date paymentDate) {
 		this.paymentDate = paymentDate;
 	}
-
 
 	public String getAddress() {
 		return address;
 	}
 
-
 	public void setAddress(String address) {
 		this.address = address;
 	}
-
 
 	public int getPhoneNo() {
 		return phoneNo;
 	}
 
-
 	public void setPhoneNo(int phoneNo) {
 		this.phoneNo = phoneNo;
 	}
-
 
 	public int getUserNo() {
 		return userNo;
 	}
 
-
 	public void setUserNo(int userNo) {
 		this.userNo = userNo;
 	}
-
 
 	public String getpName() {
 		return pName;
 	}
 
-
 	public void setpName(String pName) {
 		this.pName = pName;
 	}
-
 
 	public String getpImage1() {
 		return pImage1;
 	}
 
-
 	public void setpImage1(String pImage1) {
 		this.pImage1 = pImage1;
 	}
+
+	public int getRnum() {
+		return rnum;
+	}
+
+	public void setRnum(int rnum) {
+		this.rnum = rnum;
+	}
+
+
+	
 	
 	
 	
