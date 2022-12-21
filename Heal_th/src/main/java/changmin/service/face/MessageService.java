baa28ba7aed6f3403aa31379e27f2aa97be03ebf
Changmin <kcm2749@naver.com>
2022-12-21 19:31:02 +0900

    221221 - changmin - Feat: 메인페이지 틀 만들기

--- Heal_th/src/main/java/changmin/service/face/MessageService.java
+++ Heal_th/src/main/java/changmin/service/face/MessageService.java
@@ -6,8 +6,17 @@ import changmin.dto.MyMessage;
 
 public interface MessageService {
 
+	/**
+	 * 회원번호에 따른 메시지목록 조회
+	 * @param userno
+	 * @return
+	 */
 	public List<MyMessage> getMessageList(int userno);
 
+	/**
+	 * 회원에게 메시지 전달
+	 * @param myMessage
+	 */
 	public void pushMessage(MyMessage myMessage);
 
 }
